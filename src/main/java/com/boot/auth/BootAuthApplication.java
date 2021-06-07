421496b94cb086bbe550ec01cf61af5ad60dde29
jecho <jecho@bluetechs.com>
2021-06-07 17:10:26 +0900

    Initial commit

---
+++ src/main/java/com/boot/auth/BootAuthApplication.java
@@ -0,0 +1,13 @@
+package com.boot.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boot/auth/BootAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
